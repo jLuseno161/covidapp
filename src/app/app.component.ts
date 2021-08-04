153074d13c44ccf6e39d693355736a9d69c03fa7
ndanu-josy <josphinendanu3@gmail.com>
2021-08-04 11:53:40 +0300

    Embed map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covidapp';
+  lat = 13;
+  lng = 80;
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 38.9987208, lng: -77.2538699 },
+    zoom : 14
+  }
+  marker = {
+    position: { lat: 38.9987208, lng: -77.2538699 },
+  }
+
 }
