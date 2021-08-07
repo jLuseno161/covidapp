1356cdd6e646ad666563578337e792e08214fddb
jLuseno161 <joyluseno2@gmail.com>
2021-08-07 14:01:41 +0300

    profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
 import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
 import { SigninComponent } from './component/signin/signin.component';
 import { SignupComponent } from './component/signup/signup.component';
+import { DocDashComponent } from './component/doc-dash/doc-dash.component';
+import { PatientDashComponent } from './component/patient-dash/patient-dash.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'home', component:  LandingpageComponent},
+  { path: 'home', component: LandingpageComponent },
+  { path: 'doctor', component: DocDashComponent },
+  { path: 'patient', component: PatientDashComponent },
   // { pathMatch: 'full', redirectTo: 'home', path: '' },
   { path: '**', component: PageNotfoundComponent },
 ];
