a20acbf323e1df7755d7aec152933a4c1c343269
ndanu-josy <josphinendanu3@gmail.com>
2021-08-11 06:32:09 +0300

    changes

--- src/app/patient-input-form/patient-input-form.component.ts
+++ src/app/patient-input-form/patient-input-form.component.ts
@@ -15,6 +15,12 @@ export class PatientInputFormComponent implements OnInit {
 storedlocation: any;
 storedcoord: any;
   newInput: any = {
+    // user: null,
+    name: null,
+    symptoms: null,
+    // location: null,
+  };
+  patientInput: any = {
     user: null,
     name: null,
     symptoms: null,
@@ -34,15 +40,20 @@ storedcoord: any;
     this.storedlocation = localStorage.getItem("address");
     this.storedcoord = localStorage.getItem("coordinates");
     console.log(this.storedlocation)
-    this.user_id = localStorage.getItem('user')    
-    let {user, name, location,symptoms} = this.newInput;
-    console.log(this.newInput)
+    this.user_id = localStorage.getItem('user_id') 
+    let location= this.storedlocation 
+    let user = this.user_id  
+    console.log (user)
+    let {name,symptoms} = this.newInput;
+    this.patientInput = {user, name, location, symptoms}
+    console.log(this.patientInput)
+    
     this.resultsService.addPatient(user, name,location,symptoms ).subscribe(
       data => {
         console.log(data);
       },
     );
 
-    window.localStorage.clear(); 
+  
   }
 }
